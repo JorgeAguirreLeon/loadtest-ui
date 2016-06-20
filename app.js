6847f4a632228ce98822c6435a043d94b8483a28
jorge <jorge.aguirre.leon@gmail.com>
2016-06-20 18:45:25 +0200

    Starting POST data

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var loadtest = require('loadtest');
+var bodyParser = require('body-parser');
 
 var config = {
   "PORT": 3000
 }
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/public', express.static('public'));
 
@@ -14,7 +18,12 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('*', function(req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index', { title: 'testload-ui', message: 'Hello there!'});
+});
+
+app.post('*', function(req, res) {
+  console.log(req.body);
+  res.render('index', { title: 'Test results', message: 'Hello there!'});
 });
 
 app.listen(config.PORT, function() {
