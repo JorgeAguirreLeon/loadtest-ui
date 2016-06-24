25922f334b081fdb25da9a5c022bbdc7228067bc
jorge <jorge.aguirre.leon@gmail.com>
2016-06-24 13:01:34 +0200

    Final fixes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-testload-ui
+#testload-ui
+
+Install with `npm install`
+Run with `npm start`
+
+Easy as cake. Runs on port 3000, you may change this in *app.js*
+
+Uses _nodejs_ with _express_ and _jade_.
+
+No fancy optimizations though! And a very simple UI.
--- app.js
+++ app.js
@@ -18,12 +18,25 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('*', function(req, res) {
-  res.render('index', { title: 'testload-ui', message: 'Hello there!'});
+  res.render('index', { title: 'testload-ui', message: 'testload-ui'});
 });
 
 app.post('*', function(req, res) {
-  console.log(req.body);
-  res.render('index', { title: 'Test results', message: 'Hello there!'});
+  var options = {
+    url: req.body.url,
+    concurrency: req.body.parallelism,
+    maxRequests: req.body.requests,
+    method: req.body.method
+  };
+  loadtest.loadTest(options, function(error, result) {
+    if (error) {
+      res.render('error', {});
+    }
+    else {
+      errors = Object.keys(obj).length > 0
+      res.render('results', {title: 'Test results', message: 'testload-ui results', result: result, errors: errors});
+    }
+  });
 });
 
 app.listen(config.PORT, function() {
